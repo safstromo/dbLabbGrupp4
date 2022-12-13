46c42fe36fd7c01fa0886b5adbf17262220ca8b4
Anton <anton.haglund@iths.se>
2022-12-13 12:53:55 +0100

    Robert Test

--- src/main/java/se/iths/Main.java
+++ src/main/java/se/iths/Main.java
@@ -2,8 +2,13 @@ package se.iths;
 
 public class Main {
 
+
 	//test
     public static void main(String[] args) {
         System.out.println("Oliver är kungen!");
+    }
+    public static void print() {
+        System.out.println("Hello!");
+
     }
 }
