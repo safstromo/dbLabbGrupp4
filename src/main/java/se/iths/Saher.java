bd698ef1f17eece8837d1365c3051d10b07821e8
RehasLiamsi <nsaher91@gmail.com>
2022-12-14 13:46:53 +0100

    Saher class klass

--- src/main/java/se/iths/Saher.java
+++ src/main/java/se/iths/Saher.java
@@ -1,7 +1,28 @@
 package se.iths;
 
+import java.util.Scanner;
+
 public class Saher {
+    //TODO CLASS
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void menu() {
+
+    }
+
+    private static void detailsOfclassToInput(){
+        System.out.println("Input name of class/program: ");
+        String inputName = sc.nextLine();
+        System.out.println("Input duration of the class/program: ");
+        String durationInput = sc.nextLine();
+        System.out.println("Which school does the class/program belong to? Enter the school ID: ");
+        int schoolIDInput = sc.nextInt();
+        addNewClass(inputName, durationInput, schoolIDInput);
+        sc.nextLine();
+    }
+
+    private static void addNewClass(String inputName, String durationInput, int schoolIDInput) {
+    }
 
 
-	//TODO CLASS
 }
