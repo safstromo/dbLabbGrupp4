0540f3163cbd929174981418882a05bc367df7c7
RehasLiamsi <nsaher91@gmail.com>
2022-12-14 10:24:07 +0100

    UML diagram

--- src/main/java/se/iths/Main.java
+++ src/main/java/se/iths/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	//test
     public static void main(String[] args) {
         System.out.println("Oliver är kungen!");
+        System.out.println("Funkar för mig!");
     }
     public static void print() {
         System.out.println("Hello!");
