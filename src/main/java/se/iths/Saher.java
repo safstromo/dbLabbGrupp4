922effc47dbd7a49fa428eb3ce7f88a807f8460c
RehasLiamsi <nsaher91@gmail.com>
2022-12-15 17:40:13 +0100

    Saher add new class

--- src/main/java/se/iths/Saher.java
+++ src/main/java/se/iths/Saher.java
@@ -1,5 +1,10 @@
 package se.iths;
 
+import entity.ClassEntity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 import java.util.Scanner;
 
 public class Saher {
@@ -14,14 +19,31 @@ public class Saher {
         System.out.println("Input name of class/program: ");
         String inputName = sc.nextLine();
         System.out.println("Input duration of the class/program: ");
-        String durationInput = sc.nextLine();
+        int durationInput = sc.nextInt();
+        sc.nextLine();
         System.out.println("Which school does the class/program belong to? Enter the school ID: ");
         int schoolIDInput = sc.nextInt();
         addNewClass(inputName, durationInput, schoolIDInput);
         sc.nextLine();
     }
 
-    private static void addNewClass(String inputName, String durationInput, int schoolIDInput) {
+    private static void addNewClass(String className, int classDuration, int classSchoolID) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        ClassEntity newClass = new ClassEntity();
+
+        newClass.setClassName(className);
+        newClass.setDuration(classDuration);
+        newClass.setClassSchoolIdfk(classSchoolID);
+
+
+        entityManager.persist(newClass);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        entityManagerFactory.close();
+
+        System.out.println("Class input successful!");
     }
 
 
