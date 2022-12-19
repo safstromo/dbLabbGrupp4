f37315269bba604cf5f2b01858ae1ceddfa83f6e
Oliver <safstrom.oliver@gmail.com>
2022-12-19 08:05:08 +0100

    Todo main

--- src/main/java/se/iths/Main.java
+++ src/main/java/se/iths/Main.java
@@ -1,15 +1,20 @@
 package se.iths;
-//Todo
+/*Todo
+* MenuSystem där vi binder ihop alla klasser
+* Göra om menyer så att allt ser likt ut?
+* Göra om utskrifter så att allt ser likt ut?
+*
+
+
+*/
 public class Main {
 
 
 	//test
     public static void main(String[] args) {
-        System.out.println("Oliver är kungen!");
-        System.out.println("Funkar för mig!");
-    }
-    public static void print() {
-        System.out.println("Hello!");
+
+
 
     }
+
 }
